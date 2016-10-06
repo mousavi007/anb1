891e2a71b4d4ce92a28e4aa1e8f13287937b11da
mousavi007 <mousavi_1986@yahoo.com>
2016-10-06 18:48:10 +0330

    new add

--- resources/views/groupkala.blade.php
+++ resources/views/groupkala.blade.php
@@ -9,7 +9,7 @@
     <div class="container">
 <h1>گروه کالا</h1>
 @foreach($groupkalas as $group)
-    <div class="well wellright well-sm"> <h3> <strong>{{ $group->group }} </strong></h3> <a href="#" class="btn btn-warning" role="button">ویرایش</a> <a href="#" class="btn btn-danger" role="button">حذف</a></div>
+    <div class="well wellright well-sm"> <h3> <strong>{{ $group->group }} </strong></h3> <a href="#" class="btn btn-warning" role="button">ویرایش </a> <a href="#" class="btn btn-danger" role="button">حذف</a></div>
 
     @endforeach
 
