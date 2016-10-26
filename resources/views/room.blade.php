00d4a1c523592e6000e21d68892d6f523354440c
mousavi007 <mousavi_1986@yahoo.com>
2016-10-26 19:35:46 +0330

    new add

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -12,9 +12,11 @@
     <div class="container">
         <h1>اتاقها</h1>
         @foreach($rooms as $room)
-            <div class=" card"> <h3> <strong>{{ $room->num }} </strong></h3>
-
-                <h4> <strong> {{ $room->name()->id}}   </strong></h4><a id="myButton" href="#" class="btn btn-warning" role="button" autocomplete="off" data-toggle="modal" data-target="#login-modal5" data-whatever="">ویرایش</a> <a href="#" class="btn btn-danger" role="button" data-toggle="modal" data-target="#login-modal6" data-whatever="" >حذف</a>
+            <div class=" card"> <h3>  شماره اتاق: <strong> {{ $room->num }} </strong></h3>
+                <h4>  داخلی: <strong> {{ $room->dakheli}}   </strong></h4>
+                <h4> نوع:  <strong> {{ $room->type}}   </strong></h4>
+                <h4> <strong> {{ $room->name()->semat}}   </strong></h4>
+                <a id="myButton" href="#" class="btn btn-warning" role="button" autocomplete="off" data-toggle="modal" data-target="#login-modal5" data-whatever="{{ $room->fards_id }}">ویرایش</a> <a href="#" class="btn btn-danger" role="button" data-toggle="modal" data-target="#login-modal6" data-whatever="" >حذف</a>
             </div>
         @endforeach
 
